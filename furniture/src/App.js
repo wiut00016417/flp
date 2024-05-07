318d219cdc72161a46d6c2022ea69f77289e10cf
00016417 <wiut00016417@gmail.com>
2024-05-07 13:06:53 +0500

    Range created

--- furniture/src/App.js
+++ furniture/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
-import Banner from './components/Banner'
+import Banner from './components/Banner';
+import Range from './components/Range';
 
 function App() {
     return (
         <>
             <Header />
             <Banner />
+            <Range />
         </>
     )
 }
--- furniture/src/components/Range.jsx
+++ furniture/src/components/Range.jsx
@@ -1,11 +1,28 @@
-function Range(){
+import dining from '../assets/dining.jpg'
+import living from '../assets/living.jpg'
+import bedroom from '../assets/bedroom.png'
+
+function Ranges(){
     return (
         <div className="range">
             <h1>Browse the Range</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             <div className="gallery">
-                
+                <div className="dining">
+                    <img src={dining} alt="Dining room" />
+                    <p>Dining</p>
+                </div>
+                <div className="living">
+                    <img src={living} alt="" />
+                    <p>Living</p>
+                </div>
+                <div className="bedroom">
+                    <img src={bedroom} alt="" />
+                    <p>Bedroom</p>
+                </div>
             </div>
         </div>
     )
 }
+
+export default Ranges
