782a71ce89d6852a047f01b50053e7e558859522
00016417 <wiut00016417@gmail.com>
2024-05-08 10:59:27 +0500

    products + fonts redone

--- furniture/src/Home.jsx
+++ furniture/src/Home.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Banner from './components/Banner';
 import Range from './components/Range';
+import Products from './components/Products';
 import Footer from './components/Footer';
 
 function Home(){
@@ -9,6 +10,7 @@ function Home(){
             <Header />
             <Banner />
             <Range />
+            <Products />
             <Footer />
         </>
     )
--- furniture/src/components/Products.jsx
+++ furniture/src/components/Products.jsx
@@ -1,49 +1,72 @@
-function Producs(){
+import image1 from '../assets/productgallery/image1.png'
+import image2 from '../assets/productgallery/image2.png'
+import image3 from '../assets/productgallery/image3.png'
+import image4 from '../assets/productgallery/image4.png'
+import image5 from '../assets/productgallery/image5.png'
+import image6 from '../assets/productgallery/image6.png'
+import image7 from '../assets/productgallery/image7.png'
+import image8 from '../assets/productgallery/image8.png'
+import Button from './Button.jsx'
+
+function Products(){
     return (
         <div className="products">
             <h1>Our Products</h1>
             <div className="product-gallery">
                 <div className="product">
-                    <img src="" alt="" />
-                    <h2>Sytherine</h2>
+                    <img src={image1} alt="" />
+                    <h2>Syltherine</h2>
                     <p>Stylish cafe chair</p>
+                    <h2>2.500.000</h2>
                 </div>
                 <div className="product">
-                    <img src="" alt="" />
-                    <h2></h2>
-                    <p></p>
+                    <img src={image2} alt="" />
+                    <h2>Leviosa</h2>
+                    <p>Stylish cafe chair</p>
+                    <h2>2.500.000</h2>
                 </div>
                 <div className="product">
-                    <img src="" alt="" />
-                    <h2></h2>
-                    <p></p>
+                    <img src={image3} alt="" />
+                    <h2>Lolito</h2>
+                    <p>Luxury big sofa</p>
+                    <h2>7.000.000</h2>
                 </div>
                 <div className="product">
-                    <img src="" alt="" />
-                    <h2></h2>
-                    <p></p>
+                    <img src={image4} alt="" />
+                    <h2>Respira</h2>
+                    <p>Outdoor bar table and stool</p>
+                    <h2>500.000</h2>
                 </div>
                 <div className="product">
-                    <img src="" alt="" />
-                    <h2></h2>
-                    <p></p>
+                    <img src={image5} alt="" />
+                    <h2>Grifo</h2>
+                    <p>Night lamp</p>
+                    <h2>1.500.000</h2>
                 </div>
                 <div className="product">
-                    <img src="" alt="" />
-                    <h2></h2>
-                    <p></p>
+                    <img src={image6} alt="" />
+                    <h2>Muggo</h2>
+                    <p>Small mug</p>
+                    <h2>150.000</h2>
                 </div>
                 <div className="product">
-                    <img src="" alt="" />
-                    <h2></h2>
-                    <p></p>
+                    <img src={image7} alt="" />
+                    <h2>Mingky</h2>
+                    <p>Cute bed set</p>
+                    <h2>7.000.000</h2>
                 </div>
                 <div className="product">
-                    <img src="" alt="" />
-                    <h2></h2>
-                    <p></p>
+                    <img src={image8} alt="" />
+                    <h2>Potty</h2>
+                    <p>Minimalist flower pot</p>
+                    <h2>500.000</h2>
                 </div>
+            </div>
+            <div className="button-show">
+                <Button text={'SHOW MORE'} />
             </div>
         </div>
     )
 }
+
+export default Products
