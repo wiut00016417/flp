dd5ad553d9628ffa3b285739fa2e8f88fc40a175
wiut00016417 <wiut00016417@gmail.com>
2024-05-27 15:12:28 +0500

    'done'

--- furniture/src/components/Filter-line.jsx
+++ furniture/src/components/Filter-line.jsx
@@ -1,23 +1,29 @@
-import filter from '../assets/filter-line/filter.svg'
-import reform from '../assets/filter-line/reform.svg'
-import list from '../assets/filter-line/list.svg'
+import filter from "../assets/filter-line/filter.svg";
+import reform from "../assets/filter-line/reform.svg";
+import list from "../assets/filter-line/list.svg";
 
-export default function Filterline(){
-    return (
-        <div className="filter-line">
-            <div className="left-elements">
-                <a href=""><img src={filter} alt="filter" /></a>
-                <p>Filter</p>
-                <a href=""><img src={reform} alt="reform" /></a>
-                <a href=""><img src={list} alt="list" /></a>
-                <p>Showing 1-8 of 8 products</p>
-            </div>
-            <div className="right-elements">
-                <label>Show</label>
-                <input type="text" name="number" id="" placeholder='8' />
-                <label>Sort By</label>
-                <input type="text" name="sort" id="" placeholder='Default'/>
-            </div>
-        </div>
-    )
+export default function Filterline() {
+  return (
+    <div className="filter-line">
+      <div className="left-elements">
+        <a href="">
+          <img src={filter} alt="filter" />
+        </a>
+        <p>Filter</p>
+        <a href="">
+          <img src={reform} alt="reform" />
+        </a>
+        <a href="">
+          <img src={list} alt="list" />
+        </a>
+        <p>Showing 1-8 of 8 products</p>
+      </div>
+      <div className="right-elements">
+        <label>Show</label>
+        <input type="text" name="number" id="" placeholder="8" />
+        <label>Sort By</label>
+        <input type="text" name="sort" id="" placeholder="Default" />
+      </div>
+    </div>
+  );
 }
